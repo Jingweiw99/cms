7112764361f70885e7a321e64c13fb1fddcce581
wjw <2117678336@qq.com>
2023-11-04 22:42:43 +0800

    chore(custom): 测试commitizen和cz-customizable

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-  root: true,
+  root: true, // 表示当前目录为根目录
   env: {
-    node: true
+    node: true // node环境下启动ESLint检测
   },
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/standard'
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 'off' // error,warn,off
   }
 }
